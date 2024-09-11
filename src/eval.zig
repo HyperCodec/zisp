1aa7926a8d7bb4929cdebef6de64512d41270ca8
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2024-09-11 16:15:06 +0000

    refactor variable

--- src/eval.zig
+++ src/eval.zig
@@ -285,35 +285,6 @@ pub const StackFrame = struct {
     }
 };
 
-pub fn global_assign(_: std.mem.Allocator, args: []*model.Atom, runtime: *Runtime) !?model.Atom {
-    if (args.len != 2) {
-        return error.InvalidArgCount;
-    }
-
-    switch (args[0].*) {
-        .str => |arg1| try runtime.env.add_global(arg1.str(), args[1].*),
-        else => return error.TypeMismatch,
-    }
-
-    return null;
-}
-
-pub fn local_assign(_: std.mem.Allocator, args: []*model.Atom, runtime: *Runtime) !?model.Atom {
-    if (args.len != 2) {
-        return error.InvalidArgCount;
-    }
-
-    const name = switch (args[0].*) {
-        .str => |ident| ident.str(),
-        else => return error.TypeMismatch,
-    };
-
-    // TODO aliasing
-    try runtime.env.add_local(name, args[1].*);
-
-    return null;
-}
-
 pub fn internal_list_get(_: std.mem.Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom {
     if (args.len != 2) {
         return error.InvalidArgCount;
--- src/std/root.zig
+++ src/std/root.zig
@@ -1,15 +1,15 @@
 const Environment = @import("../eval.zig").Environment;
 const arithmetic = @import("arithmetic.zig");
 const io = @import("io.zig");
+const variable =  @import("variable.zig");
 
 pub fn setup(env: *Environment) !void {
     try arithmetic.setup(env);
     try io.setup(env);
+    try variable.setup(env);
 }
 
 // fn add_default_globals(self: *Self) !void {
-//         try self.register_internal_function("global", global_assign);
-//         try self.register_internal_function("var", local_assign);
 //         try self.register_internal_function("iget", internal_list_get);
 //         try self.register_internal_function("append", internal_list_append);
 //         try self.register_internal_function("insert", internal_list_insert);
