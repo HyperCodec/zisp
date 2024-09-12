9871fbc5f8c413e013960c4e05d58c26c8a5b891
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2024-09-12 12:52:44 +0000

    implement more comparison operators

--- src/std/conditional.zig
+++ src/std/conditional.zig
@@ -7,6 +7,10 @@ const comparator = model.TableContext{};
 
 pub fn setup(env: *eval.Environment) !void {
     try env.register_internal_function("eq", internal_eq);
+    try env.register_internal_function("neq", internal_neq);
+    try env.register_internal_function("not", internal_not);
+    try env.register_internal_function("or", internal_or);
+    try env.register_internal_function("and", internal_and);
 }
 
 pub fn internal_eq(_: Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom {
@@ -28,4 +32,48 @@ pub fn internal_eq(_: Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom 
     return model.Atom {
         .bool = true,
     };
+}
+
+pub fn internal_neq(a: Allocator, args: []*model.Atom, r: *Runtime) !?model.Atom {
+    var eq = (try internal_eq(a, args, r)).?;
+    var args2 = [_]*model.Atom{&eq};
+    return internal_not(a, args2[0..], r);
+}
+
+pub fn internal_not(_: Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom {
+    if(args.len != 1) {
+        return error.InvalidArgCount;
+    }
+
+    return switch(args[0].*) {
+        .bool => |b| model.Atom { .bool = !b },
+        else => error.TypeMismatch,
+    };
+}
+
+pub fn internal_or(_: Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom {
+    for(args) |arg| {
+        switch(arg.*) {
+            .bool => |b| if(b) {
+                // probably a bad idea but whatever
+                return arg.*;
+            },
+            else => return error.TypeMismatch,
+        }
+    }
+
+    return model.Atom { .bool = false };
+}
+
+pub fn internal_and(_: Allocator, args: []*model.Atom, _: *Runtime) !?model.Atom {
+    for(args) |arg| {
+        switch(arg.*){
+            .bool => |b| if(!b) {
+                return arg.*;
+            },
+            else => return error.TypeMismatch,
+        }
+    }
+
+    return model.Atom { .bool = true };
 }
